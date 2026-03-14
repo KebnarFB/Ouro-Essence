8ac0df861d33987cbe02149d1b1e1b81342a4617
KebnarFB <mterioprezmtz@gmail.com>
2026-03-14 17:32:56 -0600

    Se agregaron los disenios a la pagina web, asi mismo ya esta realizado bajo el framework boot spring, aun falta implementar la api.

---
+++ OuroEssence/src/main/java/Kebnar/OuroEssence/OuroEssenceApplication.java
@@ -0,0 +1,13 @@
+package Kebnar.OuroEssence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuroEssenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuroEssenceApplication.class, args);
+	}
+
+}
---
+++ OuroEssence/src/test/java/Kebnar/OuroEssence/OuroEssenceApplicationTests.java
@@ -0,0 +1,13 @@
+package Kebnar.OuroEssence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuroEssenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
